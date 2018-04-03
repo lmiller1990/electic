c58a8397e700a279fb06d31a6445581738845448
= <lachlan.miller.1990@outlook.com>
2018-04-03 22:45:24 +0900

    Add show template

--- lib/generators.ex
+++ lib/generators.ex
@@ -1,11 +1,35 @@
 defmodule Blog.Generators do
   def generate_index() do
     {:ok, files} = Blog.Files.get_all_posts(false)
-    
+
     create_links_to_each_file(files)
     |> insert_into_template
     |> write_to_index
   end
+
+  def generate_post_html(filepath) do
+    {:ok, markup} = File.read(filepath)
+    html = parse_content(markup)
+
+    write_to_file(filepath, html)
+  end
+
+  def write_to_file(filepath, content) do
+    name = String.split(filepath, "/") |> List.last |> prettify_post_name
+    html = EEx.eval_file("templates/show.html.eex", [title: name, body: content])
+
+    path = String.replace(name, " ", "_")
+
+    File.write("docs/posts/#{path}.html", html)
+  end
+
+  def parse_content(markup) do
+    case Earmark.as_html(markup) do
+      {:ok, html_doc, []} -> html_doc
+      {:error, _, errs} -> errs
+    end
+  end
+
 
   def insert_into_template(links) do
     EEx.eval_file("templates/index.html.eex", [links: links])
--- posts/tester.md
+++
@@ -1,6 +0,0 @@
-## Hello
-
-## Testing
-- one
-- two
-  - three
