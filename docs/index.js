e05105c8240a536e01bdfc441084bd2ec1d2bf66
lmiller1990 <lachlan.miller.1990@outlook.com>
2018-12-26 12:13:47 +1000

    Correct some dates and add JS to format title nicely on index

---
+++ docs/index.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const titles = document.querySelectorAll('a')
+
+  for (const title of titles) {
+    const titleText = title.text.trim()
+    const titleOnly = titleText.substr(9)
+    const formattedDate = formatDate(titleText)
+
+    title.text = `${formattedDate} - ${titleOnly}`
+  }
+})
+
+const formatDate = (title) => {
+  const date = title.substr(0, 8)
+  const year = date.substr(0, 4)
+  const month = getShortMonth(date.substr(4, 2))
+  const day = date.substr(6, 2)
+
+  return `${day} ${month} ${year}` 
+}
+
+const getShortMonth = (month) => {
+  switch (month) {
+    case '01':
+      return 'Jan'
+    case '02':
+      return 'Feb'
+    case '03':
+      return 'March'
+    case '04':
+      return 'April'
+    case '05':
+      return 'May'
+    case '06':
+      return 'June'
+    case '07':
+      return 'July'
+    case '08':
+      return 'Aug'
+    case '09':
+      return 'Sep'
+    case '10':
+      return 'Oct'
+    case '11':
+      return 'Nov'
+    case '12':
+      return 'Dec'
+  }
+}
